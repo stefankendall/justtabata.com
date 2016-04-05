7774130a8ccaa6466e736993913e253e9f5eddde
Stefan Kendall <stefankendall@gmail.com>
2016-04-04 22:31:36 -0400

    Add basic site config

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name justtabataApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the justtabataApp
+ */
+angular.module('justtabataApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name justtabataApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the justtabataApp
+ */
+angular.module('justtabataApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
