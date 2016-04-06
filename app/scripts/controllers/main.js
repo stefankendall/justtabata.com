6ad00b17c931a9110f70ecade7168f8ec25edb34
Stefan Kendall <stefankendall@gmail.com>
2016-04-06 06:35:53 -0400

    Add grayscale dependencies

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,4 @@
 'use strict';
-
-/**
- * @ngdoc function
- * @name justtabataApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the justtabataApp
- */
-angular.module('justtabataApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('app')
+    .controller('MainCtrl', function () {
+    });
